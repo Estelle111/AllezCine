77d84ee0af27f6235d3848e9e2ecd9fd0d6f2c7b
MathildeBa <baquet.mathilde@gmail.com>
2019-04-03 15:56:18 +0200

    test error

---
+++ vue/src/components/btn/Btn.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="btn">
+        <button> {{ text }} </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'btn',
+    props: ['text'],
+
+    
+}
+</script>
+
+<style>
+
+button {
+background-color: red;
+border: none;
+color: white;
+padding: 0px 55px;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 16px;
+margin: 4px 2px;
+cursor: pointer;
+}
+</style>
+
