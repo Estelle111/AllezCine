fc4cce611f871a9a4c2e33a44489a0945315cf6c
MathildeBa <baquet.mathilde@gmail.com>
2019-04-04 16:17:53 +0200

    add axios.js

---
+++ vue/src/axios.js
@@ -0,0 +1,11 @@
+import axios_http from 'axios'
+
+// insert all your axios logic here
+
+export const axios = axios_http
+
+export default {
+install (Vue, options) {
+Vue.prototype.$axios = axios_http
+}
+};
