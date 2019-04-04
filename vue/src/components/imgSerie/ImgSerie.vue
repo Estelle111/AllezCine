4653b6afadfb55dc68b2321053cd7200dd03e70f
estelle mol <estel@MacBook-Pro-de-estelle.local>
2019-04-04 16:21:20 +0200

    axios.js

--- vue/src/components/imgSerie/ImgSerie.vue
+++ vue/src/components/imgSerie/ImgSerie.vue
@@ -1,10 +1,20 @@
 <template>
+    
 </template>
 
 <script>
 export default {
   name: 'ImgSerie',
-  props: []
+  data () {
+    return {
+      serieTitle: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.themoviedb.org/3/discover/movie?api_key=7ca673fff2a5fb82abd38a9a0d559c4e&page=1')
+      .then(response => (this.serieTitle = response))
+  }
 }
 </script>
 
