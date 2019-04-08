6cc5887c5615a63cb686efc29598ee80fe091820
estelle mol <estel@MacBook-Pro-de-estelle.local>
2019-04-08 12:35:30 +0200

    genres Descriptions

--- vue/src/components/comment/Comment.vue
+++ vue/src/components/comment/Comment.vue
@@ -2,7 +2,7 @@
     <div class="commentCard">
         <div class="comment">
             <h4>
-                Tim Burton et ses acteurs fétiches
+                Lorem ipsum
             </h4>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
                 Natus ad quia sed quisquam? Totam dolores excepturi quaerat, 
